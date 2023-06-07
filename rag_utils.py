eed8210b99f37d47a451e49e18aeab36f1dffc57
moise10r <moiserushanika2006@gmail.com>
2023-06-07 02:10:00 +0200

    Set up rag llm.

---
+++ rag_utils.py
@@ -0,0 +1,62 @@
+from sentence_transformers import SentenceTransformer
+from langchain_pinecone import PineconeVectorStore
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from pinecone import Pinecone
+from openai import OpenAI
+from langchain.schema import Document
+from git import Repo
+import os
+import shutil
+import tempfile
+
+# Initialize configurations
+SUPPORTED_EXTENSIONS = {'.py', '.js', '.tsx', '.jsx', '.ipynb', '.java',
+                       '.cpp', '.ts', '.go', '.rs', '.vue', '.swift', '.c', '.h'}
+
+IGNORED_DIRS = {'node_modules', 'venv', 'env', 'dist', 'build', '.git',
+                '__pycache__', '.next', '.vscode', 'vendor'}
+
+# Initialize clients
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+pinecone_index = pc.Index("codebase-rag")
+
+# Initialize the sentence transformer model
+model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+
+def clone_repository(repo_url):
+    """Clones a GitHub repository to a temporary directory."""
+    try:
+        temp_dir = tempfile.mkdtemp()
+        Repo.clone_from(repo_url, temp_dir)
+        return temp_dir
+    except Exception as e:
+        raise Exception(f"Failed to clone repository: {str(e)}")
+
+def get_file_content(file_path, repo_path):
+    """Get content of a single file."""
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+        rel_path = os.path.relpath(file_path, repo_path)
+        return {"name": rel_path, "content": content}
+    except Exception:
+        return None
+
+def get_main_files_content(repo_path):
+    """Get content of supported code files from the local repository."""
+    files_content = []
+    for root, _, files in os.walk(repo_path):
+        if any(ignored_dir in root for ignored_dir in IGNORED_DIRS):
+            continue
+        for file in files:
+            if os.path.splitext(file)[1] in SUPPORTED_EXTENSIONS:
+                file_path = os.path.join(root, file)
+                file_content = get_file_content(file_path, repo_path)
+                if file_content:
+                    files_content.append(file_content)
+    return files_content
